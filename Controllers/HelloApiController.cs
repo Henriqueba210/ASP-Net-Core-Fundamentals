692b8fe7c2b147bfd3079969441459e08454f968
Henrique Barros de Almeida <henrique.almeida4@fatec.sp.gov.br>
2020-10-21 09:57:52 -0300

    Created class to test Api calls on C#

---
+++ Controllers/HelloApiController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CostlyNetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloApiController : ControllerBase
+    {
+
+        private readonly ILogger<HelloApiController> _logger;
+
+        public HelloApiController(ILogger<HelloApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return new JsonResult(new Result("Hello from c#"));
+        }
+    }
+
+    internal class Result
+    {
+        public string Response { get; set; }
+
+        public Result(string Value)
+        {
+            Response = Value;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,12 +14,13 @@ namespace ASP_Net_Core_Fundamentals
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _configuration = config;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration _configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -51,6 +53,8 @@ namespace ASP_Net_Core_Fundamentals
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllers();
             });
         }
     }
